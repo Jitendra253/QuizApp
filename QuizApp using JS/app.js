72f74e9790fff4b443a998daff0b20e79b1318dc
Jitendra253 <107558211+Jitendra253@users.noreply.github.com>
2022-10-20 17:49:01 +0530

    final submit

--- QuizApp using JS/app.js
+++ QuizApp using JS/app.js
@@ -1,68 +1,57 @@
-// print quiz user name
+// print quiz user name in welcome section and in question page.
 printname = () => {
-  let input = document.querySelector(".input_value").value;
-  document.querySelector("#user_message").innerHTML = "Hey!" + " " + input;
-  document.querySelector("#wish_name").innerHTML = input;
+  let user = document.querySelector(".input_value").value;
+  document.getElementById("user_message").innerHTML = "Hey!" + " " + user;
+  document.getElementById("wish_name").innerHTML = user;
 };
-// loading Main page
-let categories = document.querySelector(".welcome_section");
-categories.classList.add("activeCategories"); //show categories box
 let answer_option = document.querySelector(".answer_option");
-let mainPage = document.querySelector(".mainPage");
+let mainPage = document.getElementById("mainPage");
 let seeresult = document.querySelector(".see_result_btn");
 let question_category;
 let question_count = 0;
 let slNo = 1;
 // selecting one category category and fetch questions.
-let questions = document.querySelector("#question_page .questions");
+let questions = document.querySelector("#question_box .questions");
 let categoryButtons = document.querySelectorAll(".questionsCategory");
-let question_page = document.querySelector("#question_page");
+let question_box = document.getElementById("question_box");
+//After selecting a category remove main page and show question page
 categoryButtons.forEach((button) => {
-  button.onclick = (event) => {
+  button.addEventListener('click', (event) => {
     question_category = QUESTIONS[event.currentTarget.dataset["category"]];
     mainPage.style.display = "none";
-    categories.classList.remove("activeCategories");
-    categories.classList.remove("next");
-    question_page.removeAttribute("hidden");
+    question_box.style.display = "block"
     showQuestion(
       0,
       questions,
       QUESTIONS[event.currentTarget.dataset["category"]]
     );
-
     questionCount(1, QUESTIONS[event.currentTarget.dataset["category"]]);
-    let categoryName = document.querySelector("#categoryName");
+    let categoryName = document.getElementById("categoryName");
     categoryName.innerHTML = event.currentTarget.dataset["category"];
     // display next buton when clicken on any option. and show see result button when clicked on last question option
-    answer_option.onclick = () => {
+    answer_option.addEventListener('click', () => {
       if (question_count < question_category.length - 1) {
-        next.style.display = "block";
+        next_btn.style.display = "block";
       } else {
-        seeresult.removeAttribute("hidden");
+        seeresult.style.display = "block";
       }
-    };
+    });
     // next button for each questions and hide next question button when getting a new question
     let next_btn = document.querySelector("#next");
-    next_btn.onclick = () => {
-      next.style.display = "none";
+    next_btn.addEventListener('click', () => {
+      next_btn.style.display = "none";
       if (question_count < question_category.length - 1) {
         question_count++;
         slNo++;
         showQuestion(question_count, questions, question_category);
         questionCount(slNo, question_category);
-      } else if ((question_count = question_category.length - 1)) {
-        next.style.display = "none";
       }
-    };
-  };
+    });
+  });
 });
-let optionds = document.querySelector(".answer_option");
-//question couunter
 let option_list = document.querySelector(".answer_option");
-let question_box = document.querySelector(".question_box");
 let button = document.querySelector(".option");
-button.disable = true;
-
+//question couunter
 questionCount = (index, myApp) => {
   let top_ques_counter = question_box.querySelector(".question_count");
   let totalQuestioncount = "<span>" + index + " / " + myApp.length + "</span>";
@@ -70,11 +59,10 @@ questionCount = (index, myApp) => {
 };
 //getting question
 showQuestion = (index, elementId, myApp) => {
-  let questionNumber = myApp[index].slNo;
   let question = myApp[index].question;
   counter();
-  let que_tag = "<span>" + questionNumber + "." + question + "</span>";
-  let option_tag =
+  let show_question = "<span>" + slNo + ". " + question + "</span>";
+  let show_options =
     '<span class="option">' +
     myApp[index].options[0] +
     "</span>" +
@@ -87,8 +75,8 @@ showQuestion = (index, elementId, myApp) => {
     '<span class="option">' +
     myApp[index].options[3] +
     "</span>";
-  elementId.innerHTML = que_tag;
-  option_list.innerHTML = option_tag;
+  elementId.innerHTML = show_question;
+  option_list.innerHTML = show_options;
   // finding correct answer
   let option = option_list.querySelectorAll(".option");
   for (let i = 0; i < option.length; i++) {
@@ -98,7 +86,6 @@ showQuestion = (index, elementId, myApp) => {
 //creating scores
 let score = 0;
 let correctAns = 0;
-
 optionSelected = (answer) => {
   let userAns = answer.textContent;
   let correctAns = question_category[question_count].answer;
@@ -107,13 +94,11 @@ optionSelected = (answer) => {
   let allOptions = option_list.children.length;
   if (userAns == correctAns) {
     answer.classList.add("correct");
-    console.log("Answer is correct.");
     score++;
     scoreShow.innerHTML = "Score :" + score;
     correctAns++;
   } else {
     answer.classList.add("incorrect");
-    console.log("Answer is wrong.");
     scoreShow.innerHTML = "Score :" + score;
     //if answer is incorrect  then automatically select the correct answer
     for (let i = 0; i < allOptions; i++) {
@@ -126,12 +111,10 @@ optionSelected = (answer) => {
     option_list.children[i].classList.add("disabled");
   }
 };
-
 //timer section
 var timeleft = 300;
 var element = document.querySelector("#timer");
 var timerId = setInterval(counter, 1000);
-
 function counter() {
   if (timeleft == 00) {
     clearTimeout(timerId);
@@ -141,32 +124,29 @@ function counter() {
     timeleft--;
   }
 }
-
 //show result box after all questions
-let result_box = document.querySelector(".result_box");
+let result_box = document.getElementById("result_box");
 showResult = () => {
   mainPage.style.display = "none";
-  mainPage.classList.remove("activeInfo");
-  question_box.classList.remove("activeQuestion");
-  result_box.classList.add("activeResult");
-  //user name define in result box
+  question_box.style.display = "none"
+  result_box.style.display = "block";
+  //user name showing in result box
   let name = document.getElementById("user_name");
-  let input = document.querySelector(".input_value").value;
-  name.innerHTML = input;
+  let user = document.querySelector(".input_value").value;
+  name.innerHTML = user + "<br>";
   //Total time take for this quiz
   let time_taken = 300 - timeleft;
-  result_box.querySelector("#total_time_taken").innerHTML =
-    time_taken + " Seconds";
+  result_box.querySelector("#total_time_taken").innerHTML = "<b>Total Time Taken : </b>" + time_taken + " Seconds";
   //correct answer answered
   correct = score;
-  correctAns = document.querySelector("#correct_answers").innerHTML = correct;
+  correctAns = document.querySelector("#correct_answers").innerHTML = "<b>Correct : </b>" + correct;
   //total no of wrong answers
   let totalQuestion = 10;
   let wrong = totalQuestion - score;
-  document.querySelector("#wrong_answers").innerHTML = wrong;
+  document.querySelector("#wrong_answers").innerHTML = "<b>Wrong : </b>" + wrong;
   // score in percentage
   let percentage = (score / totalQuestion) * 100 + " %";
-  document.querySelector("#result_in_percent").innerHTML = percentage;
+  document.querySelector("#result_in_percent").innerHTML = "<b>Percentage : </b>" + percentage;
 };
 // Go back home page
 let homePage = document.querySelector(".homePage");
